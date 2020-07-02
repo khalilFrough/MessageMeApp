3fb4eb54dfcbaaf5f88bf8c043e3e295d779ecf5
Khalil <khalil_frough@yahoo.com>
2020-07-02 20:48:48 +1000

    fixed the flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:session] [:username])
         if user && user.authenticate(params[:session][:password])
             session[:user_id]= user.id
-            flash[:notice] = "You have successfuly logged in"
+            flash[:success] = "You have successfuly logged in"
             redirect_to root_path
         else
             flash.now[:error] = "There was something wrong with your login information!"
